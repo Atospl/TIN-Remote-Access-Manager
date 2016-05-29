311012003bc99b90361d233c9eefbd16f643da17
Rafał Okuniewski <rafal.okuniewski@gmail.com>
2016-05-29 17:29:31 +0200

    Added salt to regUsr.py

--- ServerApp/regUsr.py
+++ ServerApp/regUsr.py
@@ -2,12 +2,14 @@
 """App serves registering users to TIN Remote Access Manager"""
 
 import sys, getpass, hashlib, csv
-
+import string, random
 
 
 if __name__ == "__main__":
     print 'Welcome to regUsr.py - user registration tool for TIN Remote Access Manager'
     print 'Do you want to add new user? [Y/N]'
+    # salt size
+    N = 10
     response = raw_input('-->')
     while(response != 'N'):
         if(response == 'Y'):
@@ -15,7 +17,7 @@ if __name__ == "__main__":
             login = raw_input('-->')
             print("password:")
             password = getpass.getpass('-->')
-            salt = "22"
+            salt = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(N))
             
             with open('./src/config/clients', 'a') as clientsCSVFile:
                 fieldnames = ['login', 'passHash', 'salt']
